8892a46979397d039cca7b866d79b419efefa0a1
zhaosong <song0586@126.com>
2025-04-17 17:48:44 +0800

    feat: 日志配置

--- debezium-research/src/main/java/io/test/MysqlTableLockTest.java
+++ debezium-research/src/main/java/io/test/MysqlTableLockTest.java
@@ -8,6 +8,7 @@ import java.util.StringJoiner;
 public class MysqlTableLockTest {
 
     private static final String LOCK_TEMPLATE = "FLUSH TABLES %s WITH READ LOCK";
+    private static final String UNLOCK_TABLES = "UNLOCK TABLES";
     private static final List<String> SCHEMA_LIST;
 
     static {
@@ -30,10 +31,11 @@ public class MysqlTableLockTest {
     }
 
     public static void main(String[] args) throws SQLException {
-        testLockCommon(10);
+//        testLock(10);
+        testGlobalLock(10);
     }
 
-    private static void testLockCommon(int loop) {
+    private static void testLock(int loop) {
         long totalTime = 0;
         for (int i = 0; i < loop; i++) {
             String url = "jdbc:mysql://192.168.231.150:3306/testdb?characterEncoding=utf8&useSSL=false";
@@ -48,7 +50,34 @@ public class MysqlTableLockTest {
                         joiner.add(tableId);
                     }
                     stmt.execute(String.format(LOCK_TEMPLATE, joiner.toString()));
-                    stmt.execute("UNLOCK TABLES");
+                    stmt.execute(UNLOCK_TABLES);
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.printf("【x】[%s] 耗时：%sms%n", i + "", (endTime - startTime));
+            totalTime += (endTime - startTime);
+        }
+
+        System.out.printf("【x】%s次平均耗时：%sms%n", loop + "", totalTime / loop);
+    }
+
+
+    private static void testGlobalLock(int loop) {
+        long totalTime = 0;
+        for (int i = 0; i < loop; i++) {
+            String url = "jdbc:mysql://192.168.231.150:3306/testdb?characterEncoding=utf8&useSSL=false";
+            String username = "root";
+            String password = "root@123";
+            long startTime = System.currentTimeMillis();
+            try (Connection conn = DriverManager.getConnection(url, username, password);) {
+                conn.setAutoCommit(false);
+                try (Statement stmt = conn.createStatement();) {
+                    // 锁定后，无锁定超时指定，别的会话执行的修改将会一直等待
+                    stmt.execute(String.format(LOCK_TEMPLATE, ""));
+                    stmt.execute(UNLOCK_TABLES);
                 }
 
             } catch (SQLException e) {
